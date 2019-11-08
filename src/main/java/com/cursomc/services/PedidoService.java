5dea48636e7acbeba81e14a917bf5614eb7b3359
Diego <diegors90@gmail.com>
2019-11-07 22:56:43 -0300

    servico de email

--- src/main/java/com/cursomc/services/PedidoService.java
+++ src/main/java/com/cursomc/services/PedidoService.java
@@ -5,12 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cursomc.dominio.ItemPedido;
 import com.cursomc.dominio.PagamentoComBoleto;
 import com.cursomc.dominio.Pedido;
 import com.cursomc.dominio.Produto;
 import com.cursomc.dominio.enums.StatusPagamento;
+import com.cursomc.repositories.ClienteRepository;
 import com.cursomc.repositories.ItemPedidoRepository;
 import com.cursomc.repositories.PagamentoRepository;
 import com.cursomc.repositories.PedidoRepository;
@@ -30,6 +32,10 @@ public class PedidoService {
 	private PagamentoRepository pagamentoRepository;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EmailService emailService;
 	
 	public Pedido find(Integer id) {
 		Optional<Pedido> pedido = pedidoRepository.findById(id);
@@ -37,11 +43,12 @@ public class PedidoService {
 				+ id + ", Tipo: " + Pedido.class.getName()));
 	}
 	
-	
+	@Transactional
 	public Pedido insert(Pedido pedido) {
 		pedido.setData(new Date());
 		pedido.getPagamento().setStatusPagamento(StatusPagamento.PENDENTE);
 		pedido.getPagamento().setPedido(pedido);
+		pedido.setCliente(clienteRepository.findById(pedido.getCliente().getId()).get());
 		if(pedido.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagamento = (PagamentoComBoleto) pedido.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagamento, null);
@@ -58,6 +65,7 @@ public class PedidoService {
 			ip.setPedido(pedido);
 		}
 		itemPedidoRepository.saveAll(pedido.getItens());
+		emailService.sendOrderConfirmationEmail(pedido);
 		return pedido;
 	}
 }
